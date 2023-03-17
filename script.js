d262a865bc57d5eb863c0fa296bca384720f11a0
Majin-Buu611 <72584072+Majin-Buu611@users.noreply.github.com>
2023-03-17 16:44:16 -0400

    added files by uploading

---
+++ script.js
@@ -0,0 +1,72 @@
+const form = document.querySelector('form');
+const runButton = document.querySelector('#runButton');
+const downloadButton = document.querySelector('#downloadButton');
+const outputDiv = document.querySelector('#output');
+
+runButton.addEventListener('click', () => {
+    const startNum = BigInt(document.querySelector('#startNum').value);
+    const numIterations = parseInt(document.querySelector('#numIterations').value);
+
+    if (numIterations < -1) {
+        outputDiv.textContent = 'Please enter a valid number of iterations.';
+        return;
+    }
+
+    let outputStr = '';
+    let stopTime = 0;
+    let n = startNum;
+    let prevN = null;
+    let alternatingCount = 0;
+    let repeatedCount = 0;
+    let largestNum = n;
+
+    outputStr += n + '\n';
+
+    while (n !== 1n) {
+        if (numIterations === -1 || stopTime < numIterations) {
+            if (n % 2n === 0n) {
+                n /= 2n;
+            } else {
+                n = n * 3n + 1n;
+            }
+            stopTime++;
+            outputStr += n + '\n';
+
+            // Calculate pattern statistics
+            if (prevN !== null) {
+                if (prevN === n) {
+                    repeatedCount++;
+                } else {
+                    alternatingCount++;
+                }
+            }
+            prevN = n;
+
+            // Update largest number seen so far
+            if (n > largestNum) {
+                largestNum = n;
+            }
+        } else {
+            break;
+        }
+    }
+
+    outputStr += 'Stop time: ' + stopTime + '\n';
+    outputStr += 'Largest number: ' + largestNum + '\n';
+    outputStr += 'Alternating count: ' + alternatingCount + '\n';
+    outputStr += 'Repeated count: ' + repeatedCount + '\n';
+
+    outputDiv.textContent = outputStr;
+    downloadButton.disabled = false;
+    downloadButton.addEventListener('click', () => {
+        const blob = new Blob([outputStr], { type: 'text/plain' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'collatz_output.txt';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+    });
+});
